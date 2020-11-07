f9a7f792ea1d8c4a2498f85a5d3e27b931ad19f3
Borys Szygenda <szygenda.borys@gmail.com>
2020-11-07 12:10:09 +0100

    [NEW] Point checking by text init.

--- frontend/app/src/containers/LandingPage/Input/index.tsx
+++ frontend/app/src/containers/LandingPage/Input/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import UserInput from './UserInput';
 import io from 'socket.io-client';
 import { Socket } from 'socket.io-client';
+import { GameText } from '../GameText';
 
 let socket:typeof Socket;
 
@@ -9,6 +10,8 @@ export const Input:React.FunctionComponent = () => {
     // const [socket, setSocket] = useState<typeof Socket | undefined>(undefined);
     const [room, setRoomId] = useState<string>('');
     const roomId = 'testRoom123';
+
+    const [gameText, setGameText] = useState<string>('');
 
     useEffect(() => {   
         socket = io();
@@ -22,6 +25,12 @@ export const Input:React.FunctionComponent = () => {
             setRoomId(data.roomId);
         })
 
+        setGameText(
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+        );
+
         // return () => socket.disconnect();
       }, []);
 
@@ -29,16 +38,35 @@ export const Input:React.FunctionComponent = () => {
         const value = e.target.value;
         console.log(value);
         const data = {
-            char:value,
+            char:value.slice(-1),
+            at:value.length - 1,
             roomId
         }
         socket.emit('char type', data);
     }
 
     return (
-        <UserInput onChange={(e) => onSocketInput(e)} />
+        <div>
+            <GameText text={gameText} />
+            <UserInput onChange={(e) => onSocketInput(e)} />
+        </div>
     )
 
 }
 
 export default Input;
+
+// function isCharacterKeyPress(evt) {
+    //     if (typeof evt.which == "undefined") {
+    //         // This is IE, which only fires keypress events for printable keys
+    //         return true;
+    //     } else if (typeof evt.which == "number" && evt.which > 0) {
+    //         // In other browsers except old versions of WebKit, evt.which is
+    //         // only greater than zero if the keypress is a printable key.
+    //         // We need to filter out backspace and ctrl/alt/meta key combinations
+    //         return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
+    //     }
+    //     return false;
+    // }
+
+    // <input type="text" onkeypress="alert(isCharacterKeyPress(event))"></input>
